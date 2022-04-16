67089096a3871c1eb0ef3a13e17ed22c548f6b75
nico <80792499+W9K@users.noreply.github.com>
2022-04-16 03:42:54 +0200

    Create gossip.lua

---
+++ gossipgirls/gossip.lua
@@ -0,0 +1,26 @@
+UserIDS = {
+    418237714,
+}
+
+function usernames()
+	for _,Player in pairs(game:GetService('Players'):GetChildren()) do
+
+		if table.find(UserIDS, Player.UserId) then
+			if Player.Character then
+				if Player.Character.Parent.Name == 'Players' then
+					Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]' .. Player.DisplayName)
+				end
+			end
+        end
+		if Player.Character then
+			if Player.Character.Parent.Name == 'Players' then
+				if not Player.Character.LeftUpperArm:FindFirstChild('OriginalSize') then
+					Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😐]' .. Player.DisplayName)
+				end
+			end
+		end
+    end
+end
+usernames()
+
+return UserIDS
