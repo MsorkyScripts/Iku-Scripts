3b8b40fcf5dbe3352aba9a7593f1f45d31839bac
nico <80792499+W9K@users.noreply.github.com>
2022-04-16 03:48:37 +0200

    Update gossip.lua

--- gossipgirls/gossip.lua
+++ gossipgirls/gossip.lua
@@ -8,19 +8,19 @@ function usernames()
 		if table.find(UserIDS, Player.UserId) then
 			if Player.Character then
 				if Player.Character.Parent.Name == 'Players' then
-					Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]' .. Player.DisplayName)
+					Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[✔]' .. Player.DisplayName)
 				end
 			end
+        else
+            if Player.Character then
+                if Player.Character.Parent.Name == 'Players' then
+                    if not Player.Character.LeftUpperArm:FindFirstChild('OriginalSize') then
+                        Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[❌]' .. Player.DisplayName)
+                    end
+                end
+            end
         end
-		if Player.Character then
-			if Player.Character.Parent.Name == 'Players' then
-				if not Player.Character.LeftUpperArm:FindFirstChild('OriginalSize') then
-					Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😐]' .. Player.DisplayName)
-				end
-			end
-		end
     end
 end
 usernames()
-
 return UserIDS
